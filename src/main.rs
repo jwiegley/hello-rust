2793310cf23363b17bcf32fd3e834226a53b33f0
John Wiegley <johnw@newartisans.com>
2019-06-08 19:32:29 -0700

    Initialize version of stateless LTL is working

--- src/main.rs
+++ src/main.rs
@@ -1,230 +1,213 @@
-use std::env;
-use std::fs;
+// #[macro_use] extern crate lazy_static;
+extern crate regex;
 
-enum LTL<A, S> {
+use regex::Regex;
+
+// use std::env;
+use std::fmt;
+// use std::fs;
+use std::rc::Rc;
+
+enum LTL {
     Top,
     Bottom,
-    Accept(fn(A, S) -> (LTL<A, S>, S)),
-    And(LTL<A, S>, LTL<A, S>),
-    Or(LTL<A, S>, LTL<A, S>),
 
-    Next(LTL<A, S>),
+    // Accept rules take a state which is global to the aggregate LTL formula.
+    // There is no way to "scope" information using closures, such as there is
+    // in Coq or Haskell, so intermediate states must be represented the
+    // old-fashioned way.
+    Accept(fn(&str) -> Rc<LTL>),
 
-    Until(LTL<A, S>, LTL<A, S>),
-    Release(LTL<A, S>, LTL<A, S>)
+    And(Rc<LTL>, Rc<LTL>),
+    Or(Rc<LTL>, Rc<LTL>),
+
+    Next(Rc<LTL>),
+
+    Until(Rc<LTL>, Rc<LTL>),
+    Release(Rc<LTL>, Rc<LTL>),
 }
 
-enum Either<A, B> {
-    Left(A),
-    Right(B)
-}
-
-struct LTL<T>
-where T: Fn(String) -> Either<LTL<T>, Result<String>>
-{
-    step: T
-}
-
-enum Reason<A> {
-    HitBottom(String),
-    Rejected(A),
-    BothFailed(Box<Reason<A>>, Box<Reason<A>>),
-    LeftFailed(Box<Reason<A>>),
-    RightFailed(Box<Reason<A>>)
-}
-
-type Result<A> = Option<Reason<A>>;
-
-impl<T> LTL<T>
-where T: Fn(String) -> Either<LTL<T>, Result<String>>
-{
-    pub fn stop(res: Result<String>) -> LTL<T> {
-        LTL {
-            step: |_x: String| -> Either<LTL<T>, Result<String>> {
-                Right::<LTL<T>, Result<String>>(res)
-            }
+impl fmt::Display for LTL {
+    fn fmt(&self, dest: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            LTL::Top => write!(dest, "Top"),
+            LTL::Bottom => write!(dest, "Bottom"),
+            LTL::Accept(_) => write!(dest, "Accept"),
+            LTL::And(p, q) => write!(dest, "(And {} {})", p, q),
+            LTL::Or(p, q) => write!(dest, "(Or {} {})", p, q),
+            LTL::Next(p) => write!(dest, "(Next {})", p),
+            LTL::Until(p, q) => write!(dest, "(Until {} {})", p, q),
+            LTL::Release(p, q) => write!(dest, "(Release {} {})", p, q),
         }
     }
 }
 
-fn main() {
-    let args : Vec<String> = env::args().collect();
-    if args.len() <= 1 {
-        println!("usage: logscan <FILES...>");
-    }
-    let path = &args[0];
-    let contents = fs::read_to_string(path)
-        .expect("Could not read file");
-    let scanner = LTL::stop(None);
-    for line in contents.lines() {
-        (scanner.step)(line)
+enum Failed<'a> {
+    HitBottom,
+    EndOfTrace,
+    Rejected(&'a str),
+    Both(Box<Failed<'a>>, Box<Failed<'a>>),
+    Left(Box<Failed<'a>>),
+    Right(Box<Failed<'a>>),
+}
+
+impl<'a> fmt::Display for Failed<'a> {
+    fn fmt(&self, dest: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Failed::HitBottom => write!(dest, "HitBottom"),
+            Failed::EndOfTrace => write!(dest, "EndOfTrace"),
+            Failed::Rejected(reason) => write!(dest, "Rejected {}", reason),
+            Failed::Both(p, q) => write!(dest, "(Both {} {})", p, q),
+            Failed::Left(p) => write!(dest, "(Left {})", p),
+            Failed::Right(q) => write!(dest, "(Right {})", q),
+        }
     }
 }
 
-//     newtype Machine a b = Machine { step :: a -> Either (Machine a b) b }
-//
-// instance Functor (Machine a) where
-//     fmap f (Machine k) = Machine $ either (Left . fmap f) (Right . f) . k
-//
-//     run :: Machine a b -> [a] -> Either (Machine a b) b
-//     run = foldl' (either step (const . Right)) . Left
-// {-# INLINE run #-}
-//
-// data Reason a
-//     = HitBottom   String
-//     | Rejected    a
-//     | BothFailed  (Reason a) (Reason a)
-//     | LeftFailed  (Reason a)
-//     | RightFailed (Reason a)
-//     deriving (Show, Generic, NFData, Functor)
-//
-// type Result a = Maybe (Reason a)
-//
-// type LTL a = Machine a (Result a)
-//
-//     -- | ⊤, or "true"
-//     top :: LTL a
-//     top = stop Nothing
-// {-# INLINE top #-}
-//
-// -- | ⊥, or "false"
-//     bottom :: String -> LTL a
-//     bottom = stop . Just . HitBottom
-// {-# INLINE bottom #-}
-//
-// -- | Negate a formula: ¬ p
-//     neg :: LTL a -> LTL a
-//     neg = fmap invert
-// {-# INLINE neg #-}
-//
-// -- | Boolean conjunction: ∧
-//     and :: LTL a -> LTL a -> LTL a
-//     and (Machine f) g = Machine $ \a -> case f a of
-//     Right (Just e) -> Right (Just (LeftFailed e))
-//     Right Nothing  -> step g a
-//     Left f'        -> case step g a of
-//     Right (Just e) -> Right (Just (RightFailed e))
-//     Right Nothing  -> Left f'
-//     Left g'        -> Left $! f' `and` g'
-//
-//     andNext :: LTL a -> LTL a -> LTL a
-//     andNext (Machine f) g = Machine $ \a -> case f a of
-//     Right (Just e) -> Right (Just (LeftFailed e))
-//     Right Nothing  -> Left g
-//     Left f'        -> Left $! f' `and` g
-// {-# INLINE andNext #-}
-//
-// -- | Boolean disjunction: ∨
-//     or :: LTL a -> LTL a -> LTL a
-//     or (Machine f) g = Machine $ \a -> case f a of
-//     Right Nothing   -> Right Nothing
-//     Right (Just e1) -> case step g a of
-//     Right (Just e2) -> Right (Just (BothFailed e1 e2))
-//     g'              -> g'
-//     Left f' -> case step g a of
-//     Right Nothing  -> Right Nothing
-//     Right (Just _) -> Left f'
-//     Left g'        -> Left $! f' `or` g'
-//
-//     orNext :: LTL a -> LTL a -> LTL a
-//     orNext (Machine f) g = Machine $ \a -> case f a of
-//     Right Nothing  -> Right Nothing
-//     Right (Just _) -> Left g
-//     Left f'        -> Left $! f' `or` g
-// {-# INLINE orNext #-}
-//
-// invert :: Result a -> Result a
-//     invert = \case
-//     Nothing -> Just (HitBottom "neg")
-//     Just _  -> Nothing
-// {-# INLINE invert #-}
-//
-// stop :: Result a -> LTL a
-//     stop = Machine . const . Right
-// {-# INLINE stop #-}
-//
-// -- | Given an input element, provide a formula to determine its truth. These
-//     --   can be nested, making it possible to have conditional formulas. Consider
-//     --   the following:
-// --
-//     -- @
-//     -- always (accept (\n -> next (eq (succ n))))
-//     -- @
-//     --
-//     --   One way to read this would be: "for every input n, always accept n if its
-// --   next element is the successor".
-//     accept :: (a -> LTL a) -> LTL a
-//     accept f = Machine $ \a -> step (f a) a
-// {-# INLINE accept #-}
-//
-// -- | The opposite in meaning to 'accept', defined simply as 'neg . accept'.
-//     reject :: (a -> LTL a) -> LTL a
-//     reject = neg . accept
-// {-# INLINE reject #-}
-//
-// -- | The "next" temporal modality, typically written 'X p' or '◯ p'.
-//     next :: LTL a -> LTL a
-//     next = Machine . const . Left
-// {-# INLINE next #-}
-//
-// -- | The "until" temporal modality, typically written 'p U q'.
-//     until :: LTL a -> LTL a -> LTL a
-//     until p = \q -> fix $ or q . andNext p
-// {-# INLINE until #-}
-//
-// -- | Weak until.
-//     weakUntil :: LTL a -> LTL a -> LTL a
-//     weakUntil p = \q -> (p `until` q) `or` always p
-// {-# INLINE weakUntil #-}
-//
-// -- | Release, the dual of 'until'.
-//     release :: LTL a -> LTL a -> LTL a
-//     release p = \q -> fix $ and q . orNext p
-// {-# INLINE release #-}
-//
-// -- | Strong release.
-//     strongRelease :: LTL a -> LTL a -> LTL a
-//     strongRelease p = \q -> (p `release` q) `and` eventually p
-// {-# INLINE strongRelease #-}
-//
-// -- | Logical implication: p → q
-//     implies :: LTL a -> LTL a -> LTL a
-//     implies = or . neg
-// {-# INLINE implies #-}
-//
-// -- | Eventually the formula will hold, typically written F p or ◇ p.
-//     eventually :: LTL a -> LTL a
-//     eventually = until top
-// {-# INLINE eventually #-}
-//
-// -- | Always the formula must hold, typically written G p or □ p.
-//     always :: LTL a -> LTL a
-//     always = release (bottom "always")
-// {-# INLINE always #-}
-//
-// -- | True if the given Haskell boolean is true.
-//     truth :: Bool -> LTL a
-//     truth True  = top
-//     truth False = bottom "truth"
-// {-# INLINE truth #-}
-//
-// -- | True if the given predicate on the input is true.
-//     is :: (a -> Bool) -> LTL a
-//     is = accept . (truth .)
-// {-# INLINE is #-}
-//
-// -- | Another name for 'is'.
-//     test :: (a -> Bool) -> LTL a
-//     test = is
-// {-# INLINE test #-}
-//
-// -- | Check for equality with the input.
-//     eq :: Eq a => a -> LTL a
-//     eq = is . (==)
-// {-# INLINE eq #-}
-//
-// -- | Render a 'step' result as a string.
-//     showResult :: Show a => Either (LTL a) (Result a) -> String
-//     showResult (Left _)  = "<need input>"
-//     showResult (Right b) = show b
-// {-# INLINE showResult #-}
+enum Result<'a> {
+    Failure(Failed<'a>),
+    Continue(Rc<LTL>),
+    Success,
+}
+
+impl<'a> fmt::Display for Result<'a> {
+    fn fmt(&self, dest: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Result::Failure(f) => write!(dest, "Failure {}", f),
+            Result::Continue(l) => write!(dest, "Continue {}", l),
+            Result::Success => write!(dest, "Success"),
+        }
+    }
+}
+
+fn and<'a>(f: Result<'a>, g: Result<'a>) -> Result<'a> {
+    match (f, g) {
+        (Result::Failure(e), _) => Result::Failure(Failed::Left(Box::new(e))),
+        (_, Result::Failure(e)) => Result::Failure(Failed::Right(Box::new(e))),
+        (Result::Continue(f2), Result::Continue(g2)) => Result::Continue(Rc::new(LTL::And(f2, g2))),
+        (f2, Result::Success) => f2,
+        (Result::Success, g2) => g2,
+    }
+}
+
+fn or<'a>(f: Result<'a>, g: Result<'a>) -> Result<'a> {
+    match (f, g) {
+        (Result::Failure(e1), Result::Failure(e2)) => {
+            Result::Failure(Failed::Both(Box::new(e1), Box::new(e2)))
+        }
+        (Result::Success, _) => Result::Success,
+        (_, Result::Success) => Result::Success,
+        (Result::Continue(f2), Result::Continue(g2)) => Result::Continue(Rc::new(LTL::Or(f2, g2))),
+        (Result::Failure(_), g2) => g2,
+        (f2, Result::Failure(_)) => f2,
+    }
+}
+
+fn compile<'a>(l: Rc<LTL>, mx: Option<&str>) -> Result<'a> {
+    match &*l {
+        LTL::Top => Result::Success,
+        LTL::Bottom => Result::Failure(Failed::HitBottom),
+
+        LTL::Accept(v) => match mx {
+            None => Result::Success,
+            Some(x) => compile(v(x), mx),
+        },
+
+        LTL::And(p, q) => and(compile(Rc::clone(p), mx), compile(Rc::clone(&q), mx)),
+        LTL::Or(p, q) => or(compile(Rc::clone(p), mx), compile(Rc::clone(&q), mx)),
+
+        LTL::Next(p) => match mx {
+            None => compile(Rc::clone(&p), mx),
+            Some(_) => Result::Continue(Rc::clone(p)),
+        },
+
+        LTL::Until(p, q) => match mx {
+            None => compile(Rc::clone(&q), mx),
+            Some(_) => or(
+                compile(Rc::clone(&q), mx),
+                and(
+                    compile(Rc::clone(&p), mx),
+                    Result::Continue(Rc::new(LTL::Until(Rc::clone(p), Rc::clone(q)))),
+                ),
+            ),
+        },
+
+        LTL::Release(p, q) => match mx {
+            None => compile(Rc::clone(&q), mx),
+            Some(_) => and(
+                compile(Rc::clone(&q), mx),
+                or(
+                    compile(Rc::clone(&p), mx),
+                    Result::Continue(Rc::new(LTL::Release(Rc::clone(p), Rc::clone(q)))),
+                ),
+            ),
+        },
+    }
+}
+
+fn step<'a>(m: Result<'a>, x: &str) -> Result<'a> {
+    match m {
+        Result::Continue(l) => compile(Rc::clone(&l), Some(x)),
+        r => r,
+    }
+}
+
+fn run<'a>(m: Rc<LTL>, xs: &[&str]) -> Result<'a> {
+    if xs.len() == 0 {
+        compile(m, None)
+    } else {
+        match compile(m, Some(xs[0])) {
+            Result::Continue(l) => run(l, &xs[1..]),
+            r => r,
+        }
+    }
+}
+
+fn look_for_foo(text: &str) -> Rc<LTL> {
+    // lazy_static! {
+        // static ref RE: Regex = Regex::new("^foo$").unwrap();
+    // }
+    let re: Regex = Regex::new("^foo$").unwrap();
+    if re.is_match(text) {
+        Rc::new(LTL::Top)
+    } else {
+        Rc::new(LTL::Bottom)
+    }
+}
+
+fn main() {
+    // let args: Vec<String> = env::args().collect();
+    // if args.len() <= 1 {
+    //     println!("usage: logscan <FILES...>");
+    // }
+
+    // let path = &args[0];
+    // let contents = fs::read_to_string(path).expect("Could not read file");
+    let contents = "foo";
+
+    let formula = Rc::new(LTL::Accept(look_for_foo));
+    let mut st = Result::Continue(formula);
+
+    for line in contents.lines() {
+        st = step(st, line);
+    }
+
+    // run(Rc::new(LTL::Top), contents.lines());
+
+    println!("Result = {}", st)
+}
+
+#[test]
+fn main_test() {
+    let contents = "foo";
+
+    let mut formula = Result::Continue(Rc::new(LTL::Top));
+
+    for line in contents.lines() {
+        formula = step(formula, line);
+    }
+
+    // run(Rc::new(LTL::Top), contents.lines());
+
+    println!("Result = {}", formula)
+}
